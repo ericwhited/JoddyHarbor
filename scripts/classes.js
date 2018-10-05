604c808b7eaf2b3a4d3630ce872fa13740b278d4
Eric Whited <whited.j.eric@gmail.com>
2018-10-05 10:41:47 -0400

    fixed links and added js to have a pop up model

--- scripts/classes.js
+++ scripts/classes.js
@@ -1,8 +1,34 @@
-// class picker area
-var classPicker = document.getElementById("classPicker")
+// Class Picker Stuff
 
-classPicker.addEventListener("onload" animate() {
-    
+// MS2 Job Declarations 
+var zerker = document.getElementById("zerkerLink")
+var zerkerModel = document.getElementById("zerkerModel")
+
+var knight = document.getElementById("")
+// var modelBefore = document.getElementsByClassName = "classPopUpModelBefore"
+
+
+
+zerker.addEventListener("click", function(){
+    // change display to inline-block on zerker model
+    zerkerModel.classList.toggle("classPopUpModelShow")  
+    document.getElementById('modelWrap').classList.toggle('show')
 })
 
-classPicker.style.top = "0"
+
+
+// click out and exit model functionality
+document.querySelector('body').addEventListener('click', function(e) {
+    if (!e.target.classList.contains('classPhotos') && !hasSomeParentTheClass(e.target, 'classPopUpModel')) {
+        zerkerModel.classList.remove("classPopUpModelShow")  
+        document.getElementById('modelWrap').classList.remove('show')
+    }
+})
+
+// click out and exit model functionality
+function hasSomeParentTheClass(element, classname) {
+    if (element.className && element.className.split(' ').indexOf(classname)>=0) return true;
+    return element.parentNode && hasSomeParentTheClass(element.parentNode, classname);
+}
+
+
