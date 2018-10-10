e937877d585d5dec8c0c8624458b5f6575b45aa7
Eric Whited <whited.j.eric@gmail.com>
2018-10-10 17:14:23 -0400

    added more to object

--- scripts/classes.js
+++ scripts/classes.js
@@ -10,15 +10,13 @@ var hgunner = document.getElementById("hgunnerLink")
 var thief = document.getElementById("thiefLink")
 var assassin = document.getElementById("assassinLink")
 
-// models
-var knightModel = document.getElementById("knightModel")
-var zerkerModel = document.getElementById("zerkerModel")
-var priestModel = document.getElementById("priestModel")
-var wizardModel = document.getElementById("wizardModel")
-var rangerModel = document.getElementById("rangerModel")
-var hgunnerModel = document.getElementById("hgunnerModel")
-var thiefModel = document.getElementById("thiefModel")
-var assassinModel = document.getElementById("assassinModel")
+// Modal Areas
+// The background of the model left side behind the job photo
+var leftBackground = document.getElementById("left").style
+// job Photo
+var jobPhoto = document.getElementById("jobPhoto")
+// job name 
+var jobName = document.getElementById("jobName")
 
 // Gray Box to add shadow
 var greyBox = document.getElementById("greyBox")
@@ -30,13 +28,34 @@ window.onclick = function(event) {
     }
 }
 
+
+
 // Events
 knight.addEventListener("click", function(){
-    // fill code here
+    // background change (region)
+    leftBackground.backgroundImage = "url('images/backgrounds/perion.jpg')"
+    // job photo
+    jobPhoto.src = 'images/knight/Knight.png'
+    // job name
+    jobName.innerHTML = "Knight"
+    // background shade behind
     greyBox.style.display = 'block';
+    // skill area
+    // insert skills into top box
+    document.getElementById("top").after('<div>Hello</div>')
 })
 
+
+
+
+
+
 zerker.addEventListener("click", function(){
+    // background (region)
+    document.getElementById("left").style.backgroundImage = "url('images/backgrounds/perion.jpg')"
+    // job photo
+
+    // job name
     greyBox.style.display = 'block';
 })
 
@@ -65,37 +84,111 @@ assassin.addEventListener("click", function(){
 })
 
 
+var jobs = {
+    berserker: {
+        name: "Berserker",
+        photo: "place holder (photo)",
+        region: "images/zerker/zerker.png",
+        skills: [
+            darkAura = {
+                name: "Dark Aura",
+                level_requirements: "None",
+                skill_requirements: "None",
+                type: "Passive",
+                additional_types: "Dark",
+                weapon_requirements: "None",
+                cooldown: "none",
+                skill_photo: "https://c-7npsfqifvt34x24e2v6q4m5x78qbz4lx2edmpvegspoux2eofu.g00.gamepedia.com/g00/3_c-7nbqmftupsz3.hbnfqfejb.dpn_/c-7NPSFQIFVT34x24iuuqtx3ax2fx2fe2v6q4m5x78qbz4l.dmpvegspou.ofux2fnbqmftupsz3_hbnfqfejbx2ffx2ffgx2fEbsl_Bvsb.qohx3fwfstjpox3d0df7b5d21f43b7744b18edg9f0dd299ex26j21d.nbslx3djnbhf_$/$/$/$/$",
+                description: "Your latent dark power restores 10 spirit every second. Dark Aura stacks on hit, up to once per second, up to 10 times total. Each stack increases the amount restored by an additional 1 spirit." 
+            },
 
+            XSlash = {
+                name: "X Slash",
+                level_requirements: "None",
+                skill_requirements: "None",
+                type: "Active",
+                additional_types: "Close Range, Physical",
+                weapon_requirements: "Two-handed Greatsword",
+                cooldown: "none",
+                skill_photo: "https://c-7npsfqifvt34x24e2v6q4m5x78qbz4lx2edmpvegspoux2eofu.g00.gamepedia.com/g00/3_c-7nbqmftupsz3.hbnfqfejb.dpn_/c-7NPSFQIFVT34x24iuuqtx3ax2fx2fe2v6q4m5x78qbz4l.dmpvegspou.ofux2fnbqmftupsz3_hbnfqfejbx2f2x2f28x2fY_Tmbti.qohx3fwfstjpox3d39584f50b43gf9geec0993bb6f6134d3x26j21d.nbslx3djnbhf_$/$/$/$/$",
+                description: "Swing your greatsword in an X as you charge forward 4.5 m, dealing 75% damage 2 times to 5 enemies in your path. This skill can cancel other skills. You will be immune to knockback while this skill is active. Consumes 40 stamina." 
+            },
 
-// document.querySelector('body').onclick = function() {
-//     document.getElementById("show").id = "classPopUpModel"
-// }
+            deathSpin = {
+                name: "Death Spin",
+                level_requirements: "None",
+                skill_requirements: "None",
+                type: "Active",
+                additional_types: "Dark, Close Range, Physical",
+                weapon_requirements: "Two-handed Greatsword",
+                cooldown: "none",
+                skill_photo: "",
+                description: "Spin your greatsword, dealing 69% (114%) dark damage to 8 enemies within 3 m. During the attack, press a direction key to move 1.6 m. The distance decreases to 1.2 m on your second spin and to 0.8 m on your third and subsequent spins. Consumes 4 spirit." 
+            },
 
+            ragingSlash = {
+                name: "Raging Slash",
+                level_requirements: "None",
+                skill_requirements: "None",
+                type: "Active",
+                additional_types: "Dark, Close Range, Physical",
+                weapon_requirements: "Two-handed Greatsword",
+                cooldown: "none",
+                skill_photo: "",
+                description: "Swing your weapon to deal 56% (83%) dark damage to 5 enemies up to 3 m in front of you. Keep pressing the key to trigger a 3-hit combo. The attack speed increases with each hit. The third hit is a double strike, and when finished you start over from the beginning. During the attack, press a direction key to move 0.6 m." 
+            },
 
+            voidSlash = {
+                name: "Void Slash",
+                level_requirements: "Level 10",
+                skill_requirements: "None",
+                type: "Active",
+                additional_types: "Dark, Close Range, Physical",
+                weapon_requirements: "Two-handed Greatsword",
+                cooldown: "none",
+                skill_photo: "",
+                description: "Swing your greatsword upward to deal 257% (410%) dark damage to 5 enemies up to 3 m in front of you. Press the skill key again to trigger a 2-hit combo. The second part is two quick swings, dealing 97% (151%) dark damage 2 times to 5 enemies up to 2 m in front of you. On the first hit, press a direction key to move 4 m and attack. On the secnod hit, the skill lasts while the skill key is held down. You will be immune to knockback while this skill is active. The first hit consumes 24 spirit and the second hit consumes 1 spirit." 
+            },
 
-// click out and exit model functionality
-// document.querySelector('body').addEventListener('click', function(e) {
-//     if (!e.target.classList.contains('classPhotos') && !hasSomeParentTheClass(e.target, 'classPopUpModel')) {
-//         document.getElementById("show").id = "classPopUpModel"
-//         greyBox.style.visibility = 'hidden';
-//     }
-// })
+            bloodLust = {
+                name: "Blood Lust",
+                level_requirements: "Level 13",
+                skill_requirements: "Raging Slash [Level 2+]",
+                type: "Active",
+                additional_types: "Dark, Close Range, Physical",
+                weapon_requirements: "Two-handed Greatsword",
+                cooldown: "8 seconds",
+                skill_photo: "",
+                description: "Thrust your darkened greatsword into 2 enemies up to 3 m in front of you, then pull it out to deal 129% (210%) damage 2 times. The stab attack is always a critical, and pulling the sword out steals health from the enemy, restoring 7% (43%) of your max health. In PvP zones, the amount restored is halved." 
+            },
 
+            greatswordMastery = {
+                name: "Greatsword Mastery",
+                "level requirements": "Level 16",
+                skill_requirements: "Raging Slash [Level 3+]",
+                type: "Passive",
+                additional_types: "None",
+                weapon_requirements: "None",
+                cooldown: "None",
+                skill_photo: "",
+                description: "Ten's lessons on asceticism increase your skill with a greatsword. Your weapon attack increases by 1% (6.4%) when you have a greatsword equipped." 
+            },
 
-
-
-
-
-// // click out and exit model functionality
-// function hasSomeParentTheClass(element, classname) {
-//     // 
-//     if (element.className && element.className.split(' ').indexOf(classname)>=0) return true;
-//     return element.parentNode && hasSomeParentTheClass(element.parentNode, classname);
-// }
-
-
-
-
+            groundBreaker = {
+                name: "Ground Breaker",
+                level_requirements: "Level 19",
+                skill_requirements: "Raging Slash [Level 2+], Death Spin [Level 3+]",
+                type: "Active",
+                additional_types: "Close Range, Physical",
+                weapon_requirements: "Two-handed Greatsword",
+                cooldown: "None",
+                skill_photo: "",
+                description: "Thrust your darkened greatsword into 2 enemies up to 3 m in front of you, then pull it out to deal 129% (210%) damage 2 times. The stab attack is always a critical, and pulling the sword out steals health from the enemy, restoring 7% (43%) of your max health. In PvP zones, the amount restored is halved." 
+            },
+        ]
+    }
+    // next job object
+}
 
 
 
@@ -111,7 +204,8 @@ assassin.addEventListener("click", function(){
 //   }
 // }
 
-// var priestSkill1 = new mapleJobs("priest", "holy symbol", "magic stuff");
+// var darkAura = new mapleJobs("Berserker", "Dark Aura", "magic stuff");
+// var spin = new mapleJobs("Berserker", "Spin", "Spin2Win");
 
 
 // var priestSkillsButtons = document.getElementsByClassName("priestSkillButtons")
@@ -146,3 +240,7 @@ assassin.addEventListener("click", function(){
 
 //     }
 // }
+
+
+
+
